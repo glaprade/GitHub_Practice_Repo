0f6a71d113536134f0cc1a71aa40d0b80fe1c7c6
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2024-09-25 09:04:01 -0400

    Added plot feature

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
 #!/bin/python3
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 print("Hello world")
 
 
-name = input("Enter Name: ")
+#name = input("Enter Name: ")
 
-print("Hello " + name + "!")
+#print("Hello " + name + "!")
+
+x = np.array([x for x in range(20)])
+
+y = x**2
+
+plt.plot(x,y)
+plt.savefig("plot_example.png")
+
+
