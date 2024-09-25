f9c9dc1f1df76e6ec1e7b5e91ec07cd3af8a68e8
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2024-09-25 09:30:53 -0400

    Added new conflict script

---
+++ conflict.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Hello world")
+
+
+#name = input("Enter Name: ")
+
+#print("Hello " + name + "!")
+
+x = np.array([x for x in range(20)])
+
+y = x**2
+
+plt.plot(x,y)
+plt.savefig("plot_example.png")
+
+
